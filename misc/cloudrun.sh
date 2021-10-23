bff493113eda51081f0a95ced20df65619b8d813
haroldmei <haroldmei.cn@gmail.com>
2021-10-23 18:42:01 +1100

    keep it here for now

---
+++ misc/cloudrun.sh
@@ -0,0 +1,68 @@
+if [[ -z "${GCP_ACCOUNT}" ]]; then
+  echo  "GCP_ACCOUNT undefined, add 'export GCP_ACCOUNT=xxx@gmail.com' in .bashrc and source it"
+  exit 1
+fi
+
+if [[ -z "${GCLOUD_PROJECT}" ]]; then
+  echo  "GCLOUD_PROJECT undefined, add 'export GCLOUD_PROJECT=project_id' in .bashrc and source it"
+  exit 1
+fi
+
+if [[ -z "${GCS_BUCKET}" ]]; then
+  echo  "GCS_BUCKET undefined, add 'export GCS_BUCKET=bucket_url' in .bashrc and source it"
+  exit 1
+fi
+
+if [[ -z "${VM_INSTANCE}" ]]; then
+  echo  "VM_INSTANCE undefined, add 'export VM_INSTANCE=vmname' in .bashrc and source it"
+  exit 1
+fi
+
+if [[ -z "${VM_USER}" ]]; then
+  echo  "VM_USER undefined, add 'export VM_USER=user' in .bashrc and source it"
+  exit 1
+fi
+
+
+
+gcloud compute instances start $VM_INSTANCE --zone us-central1-a
+
+STATUS=$(gcloud compute ssh $VM_INSTANCE --zone=us-central1-a --ssh-flag="-q" --command 'echo 123')
+while [[ -z "$STATUS" ]]
+do
+    STATUS=$(gcloud compute ssh $VM_INSTANCE --zone=us-central1-a --ssh-flag="-q" --command 'echo 123')
+    sleep 5; 
+done
+
+WORKSPACE=$(echo $PWD | rev | cut -d'/' -f 1 | rev)
+GCSWORKSPACE=$GCS_BUCKET/$WORKSPACE
+
+#gcloud compute ssh --zone us-central1-a $VM_USER@$VM_INSTANCE --ssh-flag="-L 8080:localhost:8080"
+#gcloud compute ssh --zone us-central1-a $VM_USER@$VM_INSTANCE -- pip3 -r -s < requirements.txt
+
+# copy to gcs
+echo "sync $WORKSPACE to $GCSWORKSPACE"
+gsutil -m rsync -r . $GCSWORKSPACE
+
+# get current active account; 
+ACTIVE_ACCOUNT=$(gcloud compute ssh --zone us-central1-a $VM_USER@$VM_INSTANCE -- 'gcloud config list account --format "value(core.account)"')
+
+# login, copy data, run, upload
+if echo $ACTIVE_ACCOUNT | grep $GCP_ACCOUNT; then
+    echo "auth skipped"
+else
+    echo "auth needed"
+    gcloud compute ssh --zone us-central1-a $VM_USER@$VM_INSTANCE -- 'gcloud auth login' 
+fi
+
+COMMAND="gcloud config set project $GCLOUD_PROJECT 
+    && gsutil -m rsync -r $GCSWORKSPACE $WORKSPACE \
+    && cd $WORKSPACE \
+    && python3 $1 \
+    && gsutil -m rsync -r . $GCSWORKSPACE"
+
+gcloud compute ssh --zone us-central1-a $VM_USER@$VM_INSTANCE -- $COMMAND
+
+gsutil -m rsync -r $GCSWORKSPACE .
+
+gcloud compute instances stop $VM_INSTANCE --zone us-central1-a
